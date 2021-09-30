a441049ee3c3a8954913ba84197873f55ee3fb60
Brandon Cantello <bcantello@gmail.com>
2021-09-30 16:49:06 -0700

    adds image slider component. adds projectsHelper.js with array of images

---
+++ src/pages/projects/projectsHelper.js
@@ -0,0 +1,57 @@
+/**
+ * Array of objects projects slider. Each object includes an id, url for the image, and an alt description.
+ * @type {[{alt: string, id: number, url: string}, {}]}
+ */
+export const projectsSliderImages = [
+	[
+		{
+			id: 1,
+			url: "https://res.cloudinary.com/bcantello/image/upload/v1633044873/Screen_Shot_2021-09-30_at_4.31.52_PM_qwdfq7.png",
+			alt: "CartRover Image 1",
+		},
+		{
+			id: 2,
+			url: "https://res.cloudinary.com/bcantello/image/upload/v1633044870/Screen_Shot_2021-09-30_at_4.33.04_PM_q0sjpp.png",
+			alt: "CartRover Image 2",
+		},
+		{
+			id: 3,
+			url: "https://res.cloudinary.com/bcantello/image/upload/v1633044872/Screen_Shot_2021-09-30_at_4.33.37_PM_zp43vq.png",
+			alt: "CartRover Image 3",
+		}
+	],
+	[
+		{
+			id: 1,
+			url: "https://res.cloudinary.com/bcantello/image/upload/v1633044628/Screen_Shot_2021-09-30_at_3.53.02_PM_nrnxq1.png",
+			alt: "Spacelab Image 1",
+		},
+		{
+			id: 2,
+			url: "https://res.cloudinary.com/bcantello/image/upload/v1633044608/Screen_Shot_2021-09-30_at_3.55.19_PM_cw80cc.png",
+			alt: "Spacelab Image 2",
+		},
+		{
+			id: 3,
+			url: "https://res.cloudinary.com/bcantello/image/upload/v1633044597/Screen_Shot_2021-09-30_at_3.54.20_PM_yfxwlj.png",
+			alt: "Spacelab Image 3",
+		}
+	],
+	[
+		{
+			id: 1,
+			url: "https://res.cloudinary.com/bcantello/image/upload/v1633045569/Screen_Shot_2021-09-30_at_4.42.44_PM_tgnzse.png",
+			alt: "Peoples Voice Project Image 1",
+		},
+		{
+			id: 2,
+			url: "https://res.cloudinary.com/bcantello/image/upload/v1633045569/Screen_Shot_2021-09-30_at_4.45.28_PM_xexixl.png",
+			alt: "Peoples Voice Project Image 2",
+		},
+		{
+			id: 3,
+			url: "https://res.cloudinary.com/bcantello/image/upload/v1633045569/Screen_Shot_2021-09-30_at_4.44.20_PM_e7inec.png",
+			alt: "Peoples Voice Project Image 3",
+		}
+	]
+];
